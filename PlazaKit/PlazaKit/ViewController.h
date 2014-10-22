32afe9620cfaefe1b5624f564898de088654fa53
liyan <ujs.liyan@gmail.com>
2014-10-22 16:44:26 +0800

    成立喽

---
+++ PlazaKit/PlazaKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlazaKit
+//
+//  Created by liyan on 10/22/14.
+//  Copyright (c) 2014 Plaza66. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
